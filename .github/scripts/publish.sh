201aa15bbd06a18cbcceea4d6420f851e5288482
Eyad Abdullah <iyad1998hassan@gmail.com>
2023-08-27 20:51:07 +0200

    update workflow

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Get the version from the release tag name
-version="${{ github.event.release.tag_name }}"
+version=$1
+echo "release version: $version"
 # Update the version in build.gradle
 sed -i "s/version = '.*'/version = '$version'/" build.gradle
 # Publish to JitPack
-./gradlew clean build publish -Pgroup=com.example -Pversion=$version
+./gradlew clean build publish -Pversion=$version
